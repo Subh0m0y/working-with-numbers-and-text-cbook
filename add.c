ab8dbac08682b0e469e7bce5e97adc59524bdd9b
Subhomoy Haldar <subhomoyhaldar@gmail.com>
2017-11-12 12:51:41 +0530

    Initial Commit. Contains add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 10, b = 20;
+	double c = 2.4, d = 3.6;
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%lf + %lf = %lf\n", c, d, c + d);
+	return EXIT_SUCCESS;
+}
