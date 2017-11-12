5524b96bc8bc0fcc36aefe8b5e642e0e99c0ba88
Subhomoy Haldar <subhomoyhaldar@gmail.com>
2017-11-12 20:17:04 +0530

    Updated add.c

--- add.c
+++ add.c
@@ -5,7 +5,9 @@ int main()
 {
 	int a = 10, b = 20;
 	double c = 2.4, d = 3.6;
-	printf("%d + %d = %d\n", a, b, a + b);
-	printf("%lf + %lf = %lf\n", c, d, c + d);
+	int sum1 = a + b;
+	double sum2 = c + d;
+	printf("%d + %d = %d\n", a, b, sum1);
+	printf("%lf + %lf = %lf\n", c, d, sum2);
 	return EXIT_SUCCESS;
 }
